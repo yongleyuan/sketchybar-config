8f0cca48c7c57bbbd98cf5ee6b71b869ecf85e73
yongleyuan <yongle.jack.yuan@gmail.com>
2025-04-26 01:19:12 -0400

    sketchybar: change workspace settings

--- items/aerospace.sh
+++ items/aerospace.sh
@@ -22,26 +22,6 @@ for sid in ${sids1[@]}; do
                   label.padding_right=8 \
                   label.color="0xaa$LAVENDER" \
                   label.highlight_color="0xffffffff" \
-                  display=1 \
-                  click_script="aerospace workspace $sid" \
-                  script="$PLUGIN_DIR/aerospace.sh $sid"
-done
-
-sids2=("1" "2" "3" "4" "5")
-for sid in ${sids2[@]}; do
-  $BAR_NAME --add item aerospace.$sid left \
-            --subscribe aerospace.$sid aerospace_workspace_change \
-            --set aerospace.$sid \
-                  background.padding_right=10 \
-                  icon.drawing=off \
-                  padding_left=4 \
-                  padding_right=4 \
-                  label="$sid" \
-                  label.padding_left=8 \
-                  label.padding_right=8 \
-                  label.color="0xaa$LAVENDER" \
-                  label.highlight_color="0xffffffff" \
-                  display=2 \
                   click_script="aerospace workspace $sid" \
                   script="$PLUGIN_DIR/aerospace.sh $sid"
 done
@@ -54,22 +34,4 @@ $BAR_NAME --add bracket spaces '/aerospace\..*/' \
                 background.border_width=0 \
                 background.padding_left=0 \
                 background.padding_right=0
-
-# $BAR_NAME --add item aerospace left \
-#           --subscribe aerospace aerospace_workspace_change \
-#           --set aerospace \
-#                 icon="" \
-#                 icon.font.size=15 \
-#                 icon.color="0xff$LAVENDER" \
-#                 icon.padding_right=6 \
-#                 label=$(aerospace list-workspaces --focused) \
-#                 label.font.size=16 \
-#                 background.height=24 \
-#                 background.corner_radius=5 \
-#                 background.drawing=on \
-#                 background.border_width=0 \
-#                 background.padding_left=0 \
-#                 background.padding_right=0 \
-#                 display=active \
-#                 script="$PLUGIN_DIR/aerospace.sh"
 
